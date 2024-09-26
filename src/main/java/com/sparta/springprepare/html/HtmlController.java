d23aec30d835e5b68c870e61314009f301eb4dd3
wootaepark <pakdal99@gmail.com>
2024-09-26 12:29:49 +0900

    feat : static page & dynamic page with Thymeleaf

---
+++ src/main/java/com/sparta/springprepare/html/HtmlController.java
@@ -0,0 +1,37 @@
+package com.sparta.springprepare.html;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HtmlController {
+
+    private static long visitCount = 0; // 방문 횟수
+
+
+    @GetMapping("/static-hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/html/redirect")
+    public String htmlStatic() {
+        return "redirect:/hello.html";
+    }
+
+    @GetMapping("/html/templates")
+    public String htmlTemplates() {
+        return "hello";
+    }
+
+    @GetMapping("/html/dynamic")
+    public String htmlDynamic(Model model) {
+        visitCount++;
+        model.addAttribute("visits", visitCount);
+        return "hello-visit";
+    }
+
+
+}
